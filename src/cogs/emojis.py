1be3c4cbc583392f9bc480d99355fefd6c724498
Teodor de Fine Licht <Teolicht@TeodorLs-MacBook-Pro.local>
2018-07-06 14:16:07 -0300

    Initial commit

---
+++ src/cogs/emojis.py
@@ -0,0 +1,12 @@
+class Emoji:
+    cat = '\U0001f431'
+    dog = '\U0001f436'
+    warning = '\U000026a0'
+    error = '\U0000274c'
+    pingpong = '\U0001f3d3'
+    online = "<:ON:425085623490052096>"
+    offline = "<:OFF:425085623150444565>"
+    idle = "<:IDLE:425085623183998986>"
+    dnd = "<:DND:425085623028678657>"
+    awesome = "<:awesome:388415877806293002>"
+    python = "<:python3:232720527448342530>"
