61db3d2dc2c8ad7b2a5edfc1c3b887c5fdc63300
teolicht <teodor@musicmedia.dk>
2021-05-04 20:25:59 -0300

    Move bot invite link to json file

--- src/cogs/invites.py
+++ src/cogs/invites.py
@@ -1,6 +1,10 @@
 from discord.ext import commands
 import discord
+import json
+import os
 
+
+CONFIG = json.load(open(os.path.join(os.path.dirname(__file__), "text", "config.json"), 'r'))
 
 class Invites(commands.Cog):
 	def __init__(self, bot):
@@ -8,8 +12,7 @@ class Invites(commands.Cog):
 
 	@commands.command()
 	async def invite(self, ctx):
-		await ctx.send("**https://discordapp.com/api/oauth2/authorize?" +
-			"client_id=465945853220093954&permissions=8&scope=bot**")
+		await ctx.send(CONFIG["invite"])
 
 	@commands.command()
 	async def svinvite(self, ctx):
