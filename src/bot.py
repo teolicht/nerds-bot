5399c77ad4b8e9daf5efec439a1f7e99f73e3467
teolicht <teodor@musicmedia.dk>
2023-04-23 18:15:29 -0300

    Move poll command to utilities cog

--- src/bot.py
+++ src/bot.py
@@ -22,18 +22,17 @@ initial_extensions = [
     "cogs.moderation",
     "cogs.pictures",
     "cogs.reddit",
-    "cogs.utilities"
+    "cogs.utilities",
 ]
 
 
 class NerdsBot(commands.Bot):
-
     def __init__(self):
         super().__init__(
             command_prefix=command_prefix,
             description=description,
             help_command=None,
-            intents=intents
+            intents=intents,
         )
 
     async def setup_hook(self) -> None:
@@ -103,6 +102,7 @@ class NerdsBot(commands.Bot):
 # NerdsBot instance
 bot = NerdsBot()
 
+
 @bot.command()
 async def sync(ctx):
     if ctx.author.id != settings.NERDS_MEMBERS[0]:
@@ -110,177 +110,6 @@ async def sync(ctx):
     synced = await ctx.bot.tree.sync()
     await ctx.send(f":white_check_mark: Synced `{len(synced)}` commands")
 
-# This command shall be rewritten and moved to utilities cog
-@bot.command()
-async def poll(
-    # fmt: off
-    ctx, question, duration: int, option1, option2,
-    option3=None, option4=None, option5=None, option6=None,
-    option7=None, option8=None, option9=None, option10=None,
-):
-    # fmt: on
-    await ctx.message.delete()
-    initial_options = [
-        option1,
-        option2,
-        option3,
-        option4,
-        option5,
-        option6,
-        option7,
-        option8,
-        option9,
-        option10,
-    ]
-    options = []
-    for option in initial_options:
-        if option is not None:
-            options.append(option)
-
-    ones, twos, threes, fours, fives = 0, 0, 0, 0, 0
-    sixs, sevens, eights, nines, tens = 0, 0, 0, 0, 0
-
-    if len(options) < 2:
-        return await ctx.send(":x: At least 2 options needed.")
-    elif len(options) > 10:
-        return await ctx.send(":x: Max 10 options allowed.")
-
-    reactions = ["1⃣", "2⃣", "3⃣", "4⃣", "5⃣", "6⃣", "7⃣", "8⃣", "9⃣", "🔟"]
-
-    description = []
-    for x, option in enumerate(options):
-        description += "\n{} {}\n\u200b".format(reactions[x], option)
-    em = discord.Embed(title=question, description="".join(description))
-    em.set_author(name=f"{ctx.author.name}'s poll",
-                    icon_url=ctx.author.display_avatar)
-    react_msg = await ctx.send(embed=em)
-
-    for reaction in reactions[: len(options)]:
-        await react_msg.add_reaction(reaction)
-
-    while duration > 0:
-        if duration > 1:
-            second = "seconds"
-        else:
-            second = "second"
-        em.set_footer(
-            text="This poll will end in {} {}.".format(duration, second))
-        await react_msg.edit(embed=em)
-
-        await asyncio.sleep(1)
-        duration -= 1
-
-    await react_msg.edit(embed=em)
-
-    onesV, twosV, threesV, foursV, fivesV = [], [], [], [], []
-    sixsV, sevensV, eightsV, ninesV, tensV = [], [], [], [], []
-    numbers = [ones, twos, threes, fours, fives,
-                sixs, sevens, eights, nines, tens]
-    voters = [
-        onesV,
-        twosV,
-        threesV,
-        foursV,
-        fivesV,
-        sixsV,
-        sevensV,
-        eightsV,
-        ninesV,
-        tensV,
-    ]
-
-    cache_msg = await react_msg.channel.fetch_message(react_msg.id)
-    for reaction in cache_msg.reactions:
-        async for user in reaction.users():
-            if user != bot.user:
-                for x, voter in enumerate(voters):
-                    if reaction.emoji == reactions[x]:
-                        numbers[x] += 1
-                        voter.append(user.mention)
-
-    highest_num = max(numbers)
-    highest_nums = []
-    for num in numbers:
-        if num == highest_num:
-            highest_nums.append(num)
-    if len(highest_nums) > 1:
-        winner_option = False
-        tie_options = []
-        try:
-            for x, num in enumerate(numbers):
-                if num == highest_nums[0]:
-                    tie_options.append(options[x])
-        except IndexError:
-            em.set_footer(text="❌ No one voted.")
-            await react_msg.edit(embed=em)
-            for reaction in cache_msg.reactions:
-                async for user in reaction.users():
-                    await cache_msg.remove_reaction(reaction, user)
-            return
-
-        tie_winner = random.choice(tie_options)
-        em.set_footer(text="It's a tie! Picking a random winner...")
-        await react_msg.edit(embed=em)
-
-    else:
-        for x, num in enumerate(numbers):
-            if num == highest_nums[0]:
-                winner_option = options[x]
-        em.set_footer(text="❌ This poll has ended.")
-        await react_msg.edit(embed=em)
-
-    for reaction in cache_msg.reactions:
-        async for user in reaction.users():
-            await cache_msg.remove_reaction(reaction, user)
-
-    format1 = "**{}**\n └ {}"  # To be used the option has at least one voter
-    format2 = "**{}**"  # To be used if the option has no voters
-    results = []
-    for x, voter in enumerate(voters):
-        voters[x] = ", ".join(voters[x])
-        if numbers[x] > 0:
-            results.append(format1.format(numbers[x], voters[x]))
-        else:
-            results.append(format2.format(numbers[x]))
-
-    total = []
-    for x, option in enumerate(options):
-        total += "\n`{}`: {}\n".format(option, results[x])
-
-    if winner_option:
-        em = discord.Embed(
-            title=question,
-            description="__Result__{}\n:star: {}".format(
-                "".join(total), winner_option),
-        )
-    else:
-        em = discord.Embed(
-            title=question, description="__Result__{}".format(
-                "".join(total))
-        )
-    em.set_author(name=f"{ctx.author.name}'s poll",
-                    icon_url=ctx.author.display_avatar)
-    if winner_option:
-        em.set_footer(text="❌ This poll has ended.")
-        await react_msg.edit(embed=em)
-    else:
-        em.set_footer(text="It's a tie! Picking a random winner...")
-        await asyncio.sleep(3)
-        await react_msg.edit(embed=em)
-
-        em = discord.Embed(
-            title=question,
-            description="__Result__{}\n:star: {}".format(
-                "".join(total), tie_winner),
-        )
-        em.set_author(
-            name=f"{ctx.author.name}'s poll", icon_url=ctx.author.display_avatar
-        )
-        em.set_footer(text="❌ This poll has ended.")
-        await react_msg.edit(embed=em)
-
 
 # Maybe move this to a launcher file at some point?
 asyncio.run(bot.start())
-
-
